4bd8c55c73c5324862a77491dc4fc3db9fae6aee
Nikita <nikita.vinitskov@gmail.com>
2020-12-27 17:53:11 +0100

    lesson functions

--- js/test.js
+++ js/test.js
@@ -1,5 +1,17 @@
 'use strict';
 
-for (let i = 1; i < 0; i++) {
-    console.log(i);
+const arr = [];
+
+for (let i = 1; i < 10; i++) {
+    arr.push(Math.round(Math.random() * 10) + 1);
 }
+
+console.log(arr);
+
+const arr1 = [];
+
+for (let i = 1; i <= 10; i++) {
+    arr1.push(Math.random());
+}
+
+console.log(arr1);
