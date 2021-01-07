8fea412b282e902479bba2db26c974bf227aa8b0
Nikita <nikita.vinitskov@gmail.com>
2021-01-07 12:45:06 +0100

    add object methods

--- js/test.js
+++ js/test.js
@@ -1,17 +1,12 @@
 'use strict';
 
-const arr = [];
 
-for (let i = 1; i < 10; i++) {
-    arr.push(Math.round(Math.random() * 10) + 1);
+function copy(objMain) {
+    let objCopy = {};
+
+    let key;
+    for (key in objMain) {
+        objCopy[key] = objMain[key];
+    }
+    return objCopy;
 }
-
-console.log(arr);
-
-const arr1 = [];
-
-for (let i = 1; i <= 10; i++) {
-    arr1.push(Math.random());
-}
-
-console.log(arr1);
